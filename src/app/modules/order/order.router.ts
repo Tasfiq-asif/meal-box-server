ccd6830b471611d9cb26a4e5af115b9c0754f37c
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2025-04-25 21:21:42 +0600

    refactor order module

--- src/app/modules/order/order.router.ts
+++ src/app/modules/order/order.router.ts
@@ -7,6 +7,8 @@ const router = Router();
 // Route to create a new order - only customers can create orders
 router.post("/customers/order", auth("customer"), OrderController.createOrder);
 
+router.patch("/orders/:orderId", OrderController.updateOrder);
+
 // Route to get orders for a specific customer - only accessible by customers or the specific customer
 router.get(
   "/customers/orders/:customerId",
